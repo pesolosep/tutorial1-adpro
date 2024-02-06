56d7007deff2ab5efdf91d76ccbaecc57bba740e
Faishal Nelwan <108632813+pesolosep@users.noreply.github.com>
2024-02-07 00:15:47 +0700

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,23 @@
+
+
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm")
 }
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.1"
+val webdrivermanagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
+
+
 
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
 }
 
 configurations {
@@ -29,8 +38,30 @@ dependencies {
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
+	testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
+	testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+	implementation(kotlin("stdlib-jdk8"))
 }
 
-tasks.withType<Test> {
+tasks.register<Test>("unitTest"){
+	description = "Runs unit tests."
+	group ="verification"
+	filter{
+		excludeTestsMatching("*FunctionalTest")
+	}
+}
+
+tasks.register<Test>("functionalTest"){
+	description = "Runs functional tests."
+	group = "verification"
+
+	filter{
+		includeTestsMatching("*FunctionalTest")
+	}
+}
+tasks.withType<Test>().configureEach {
 	useJUnitPlatform()
 }
