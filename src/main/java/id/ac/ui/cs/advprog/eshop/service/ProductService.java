67ee72768af7d59242e29b99db0f5891d693d36d
Faishal Nelwan <108632813+pesolosep@users.noreply.github.com>
2024-02-06 23:57:50 +0700

    create ProductService and ProductServiceImpl

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.eshop.service;
+import java.util.List;
+import id.ac.ui.cs.advprog.eshop.model.Product;
+public interface ProductService {
+    public Product create(Product product);
+    public List<Product> findAll();
+    }
