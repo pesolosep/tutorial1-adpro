b58743b48d52696c59e09bfadd7cd01a3e336648
Faishal Nelwan <108632813+pesolosep@users.noreply.github.com>
2024-02-07 17:42:40 +0700

    finish reflection

--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
@@ -17,7 +17,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import java.util.List;
 
 @SpringBootTest(
         webEnvironment = WebEnvironment.RANDOM_PORT
