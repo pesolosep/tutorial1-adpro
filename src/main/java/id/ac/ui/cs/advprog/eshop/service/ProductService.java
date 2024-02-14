e993e3e4e2e6ea7846c2bb345c5c6cc26ea8732f
Faishal Nelwan <108632813+pesolosep@users.noreply.github.com>
2024-02-14 15:34:18 +0700

    finish add more Test (100% coverage)

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,7 +4,7 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 public interface ProductService {
     public Product create(Product product);
     public List<Product> findAll();
-    public void delete(String productId); // Add this line
+    public Product delete(String productId); // Add this line
     public Product findById(String productId);
     public Product edit(String productId, Product product);
 }
